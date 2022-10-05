dcf76670b334c0467a68238b976a34957d770021
Efe Baslılar <hopa_efe_08@hotmail.com>
2022-10-05 22:40:08 +0300

    05.10.2022 22.39 (FKategoriList TreeView Yapısı Kurgulandı.)

--- MEYPAK.PRL/STOK/FKategoriList.Designer.cs
+++ MEYPAK.PRL/STOK/FKategoriList.Designer.cs
@@ -71,7 +71,7 @@
             this.button3.TabIndex = 5;
             this.button3.Text = "Alt Kategori Ekle";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+           // this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button1
             // 
--- MEYPAK.PRL/STOK/FKategoriList.cs
+++ MEYPAK.PRL/STOK/FKategoriList.cs
@@ -9,20 +9,22 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace MEYPAK.PRL.STOK
 {
     public partial class FKategoriList : Form
     {
-        
+
         int id;
         string _islem;
-        public FKategoriList(string islem="")
+        public FKategoriList(string islem = "")
         {
             InitializeComponent();
             this._islem = islem;
@@ -30,67 +32,121 @@ namespace MEYPAK.PRL.STOK
         IKategoriServis _kategoriServis = new KategoriManager(new EFKategoriRepo(NinjectFactory.CompositionRoot.Resolve<MEYPAKContext>()));
         private void FKategoriList_Load(object sender, EventArgs e)
         {
+            
+            MPKATEGORI A = new MPKATEGORI()
+            {
+                Acıklama = "Ceri",
+                UstId = 2
+            };
+            _kategoriServis.Ekle(A);
+            MPKATEGORI b = new MPKATEGORI()
+            {
+                Acıklama = "Meyve",
+                UstId = 0
+            };
+            _kategoriServis.Ekle(b);
+
+            MPKATEGORI c = new MPKATEGORI()
+            {
+                Acıklama = "Cilek",
+                UstId = 4
+            };
+            _kategoriServis.Ekle(c);
             TreeViewDoldur();
         }
 
         void TreeViewDoldur()
         {
-            var data = _kategoriServis.Listele();
+            var data = _kategoriServis.Getir(x => x.UstId == 0); // Elektronik, Bahçe...
             if (data != null)
             {
-                List < TreeNode > treenodes = new List<TreeNode>();
-                treenodes= TreeViewListele(treenodes, data);
-                TreeNode[] treearray = new TreeNode[treenodes.Count];
-                
-                TreeNode node = new TreeNode("Kategoriler",treearray);
-                treeView1.Nodes.Add(node);
+                foreach (var item in data) // üst kategori
+                {
+                    TreeNode ustNode = new TreeNode(item.Acıklama);
+                    ustNode.Tag = item;
+                    treeView1.Nodes.Add(ustNode);
+                    if (item.UstId==0 
+                      && _kategoriServis.Getir(x => x.UstId == item.ID).Count > 0)
+                    {
+                        bool isOver = false;
+                        MPKATEGORI currentBaseCategory = item;
+
+                        while (!isOver)
+                        {
+                            if (_kategoriServis.Getir(x => x.UstId == currentBaseCategory.ID).Count > 0)
+                            {
+                                // mevcuttaki ana kategorinin üst kategorisi varmış
+                                // onu alalım
+                                var altData = _kategoriServis.Getir(x => x.UstId == currentBaseCategory.ID);
+                                currentBaseCategory = _kategoriServis.Getir(x => x.UstId == currentBaseCategory.ID).FirstOrDefault();
+                                //TEKRAR BAŞA DÖNMELİ..
+                                TreeViewDon(ref ustNode, altData);
+                                // ustnode değişmeli
+                            }
+                            else
+                            {
+                                isOver = true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            void TreeViewDon(ref TreeNode ustNode, List<MPKATEGORI> data)
+            {
+                foreach (var item in data)
+                {
+                    TreeNode altNode = new TreeNode(item.Acıklama);
+                    altNode.Tag = item;
+                    ustNode.Nodes.Add(altNode);
+
+                }
 
             }
-        }
 
-        List<TreeNode> TreeViewListele(List<TreeNode> node,List<MPKATEGORI> _tempKategori)
-        {
-            foreach (MPKATEGORI item in _tempKategori)
+            List<TreeNode> TreeViewListele(List<TreeNode> node, List<MPKATEGORI> _tempKategori)
             {
-                if (item.UstId == 0)
+                foreach (MPKATEGORI item in _tempKategori)
                 {
-                    TreeNode treeNode = new TreeNode();
-                    treeNode.Nodes.Add(item.Acıklama);
-                    treeNode.Tag = item.ID;
-                    node.Add(treeNode);
-                    var altdata = _kategoriServis.Getir(x => x.UstId == item.ID);
-                    return TreeViewListele(node, altdata);
+                    if (item.UstId == 0)
+                    {
+                        TreeNode treeNode = new TreeNode();
+                        treeNode.Nodes.Add(item.Acıklama);
+                        treeNode.Tag = item.ID;
+                        node.Add(treeNode);
+                        var altdata = _kategoriServis.Getir(x => x.UstId == item.ID);
+                        return TreeViewListele(node, altdata);
+                    }
+                    else
+                    {
+                        TreeNode treeNode = new TreeNode();
+                        treeNode.Nodes.Add(item.Acıklama);
+                        treeNode.Tag = item.ID;
+                        node.Add(treeNode);
+                        var altdata = _kategoriServis.Getir(x => x.UstId == item.ID);
+                        return TreeViewListele(node, altdata);
+                    }
+                }
+                return node;
+
+            }
+
+             void button3_Click(object sender, EventArgs e)
+            {
+                if (treeView1.SelectedNode.Tag != null)
+                {
+                    MPKATEGORI mPKATEGORI = new MPKATEGORI()
+                    {
+                        UstId = (int)treeView1.SelectedNode.Tag,
+                        Acıklama = textBox1.Text
+                    };
+                    _kategoriServis.Ekle(mPKATEGORI);
+                    treeView1.Nodes.Clear();
                 }
                 else
                 {
-                    TreeNode treeNode = new TreeNode();
-                    treeNode.Nodes.Add(item.Acıklama);
-                    treeNode.Tag = item.ID;
-                    node.Add(treeNode);
-                    var altdata = _kategoriServis.Getir(x => x.UstId == item.ID);
-                    return TreeViewListele(node, altdata);
+                    MessageBox.Show("Lütfen alt kategori eklemek istediğiniz kategoriyi seçiniz.");
                 }
             }
-            return node;
-
         }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            if (treeView1.SelectedNode.Tag !=null)
-            {
-                MPKATEGORI mPKATEGORI = new MPKATEGORI()
-                {
-                    UstId = (int)treeView1.SelectedNode.Tag,
-                    Acıklama = textBox1.Text
-                };
-                _kategoriServis.Ekle(mPKATEGORI);
-                treeView1.Nodes.Clear();
-            }
-            else
-            {
-                MessageBox.Show("Lütfen alt kategori eklemek istediğiniz kategoriyi seçiniz.");
-            }
-        }
-    }
-}
+    } }
