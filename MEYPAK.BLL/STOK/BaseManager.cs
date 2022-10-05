1a778d18be2a7f8698c899b31f6965e86f60d585
efebaslilar <efebaslilar@gmail.com>
2022-10-05 09:16:39 +0300

    09.16.2022 09.16 (BaseManager)

---
+++ MEYPAK.BLL/STOK/BaseManager.cs
@@ -0,0 +1,64 @@
+﻿using MEYPAK.DAL.Abstract;
+using MEYPAK.Entity.Models;
+using MEYPAK.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEYPAK.BLL.STOK
+{
+    public class BaseManager<T> : IGenericServis<T>
+    {
+        IGeneric<T> _generic;
+
+        public BaseManager(IGeneric<T> generic)
+        {
+            this._generic = generic;
+
+        }
+
+        public Interfaces.Durum Ekle(T entity)
+        {
+            return _generic.Ekle(entity);
+        }
+
+        public Durum EkleyadaGuncelle(T entity)
+        {
+            return _generic.EkleyadaGuncelle(entity);
+        }
+
+        public List<T> Getir(string entity)
+        {
+            return _generic.Getir(entity);
+        }
+
+        public List<T> Getir(Expression<Func<T, bool>> predicate)
+        {
+            return _generic.Getir(predicate);
+        }
+
+        public List<T> Guncelle(T entity)
+        {
+            return _generic.Guncelle(entity);
+        }
+
+        public List<T> Listele()
+        {
+            return _generic.Listele();
+        }
+
+        public bool Sil(System.Linq.Expressions.Expression<Func<T, bool>> predicate)
+        {
+            return _generic.Sil(predicate);
+        }
+
+        public bool Sil(List<T> entity)
+        {
+            return _generic.Sil(entity);
+        }
+
+    }
+}
