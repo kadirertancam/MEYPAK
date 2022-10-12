7f1ca177ba82712935ec29d1828467152f9c41e3
efebaslilar <efebaslilar@gmail.com>
2022-10-12 16:28:56 +0300

    12.10.2022 16.28 // MEYPAK.WEB Başlangıç Yapıldı.

---
+++ MEYPAK.WEB/Controllers/StokController.cs
@@ -0,0 +1,37 @@
+﻿using MEYPAK.DAL.Concrete.EntityFramework.Context;
+using Microsoft.AspNetCore.Mvc;
+using MEYPAK.DAL.Concrete.EntityFramework.Repository;
+using MEYPAK.DAL.Concrete.EntityFramework.Context;
+using MEYPAK.BLL.STOK;
+using MEYPAK.Entity.Models;
+using MEYPAK.Interfaces.Stok;
+
+namespace MEYPAK.WEB.Controllers
+{
+    public class StokController : Controller
+
+    {
+        static MEYPAKContext context = new MEYPAKContext();
+
+        private readonly IStokServis _stokServis = new StokManager(new EFStokRepo(context));
+        private readonly ILogger<StokController> _logger;
+
+        public StokController(ILogger<StokController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public IActionResult StokKart()
+        {
+           
+                List<MPSTOK> data = _stokServis.Listele();
+                return View(data); 
+        }
+        [HttpPost]
+        public IActionResult StokKart(List<MPSTOK> mPSTOKs)
+        {
+            return View();
+        }
+    }
+}
