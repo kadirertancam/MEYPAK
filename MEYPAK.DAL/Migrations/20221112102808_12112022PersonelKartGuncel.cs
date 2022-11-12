7b10810555aa41f9c3a4fa9c5f7c00eeca6f4e56
seydayildiz <seydayildiz1206@gmail.com>
2022-11-12 13:28:55 +0300

    12.11.2022 13:28 //Migration İşlemi

---
+++ MEYPAK.DAL/Migrations/20221112102808_12112022PersonelKartGuncel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MEYPAK.DAL.Migrations
+{
+    public partial class _12112022PersonelKartGuncel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CİLTNO",
+                table: "MPPERSONEL");
+
+            migrationBuilder.RenameColumn(
+                name: "GOREVKODU",
+                table: "MPPERSONEL",
+                newName: "MESLEKKODU");
+
+            migrationBuilder.RenameColumn(
+                name: "GOREVI",
+                table: "MPPERSONEL",
+                newName: "CILTNO");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "NUFUSCUZDANVERILISTARIH",
+                table: "MPPERSONEL",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MESLEKKODU",
+                table: "MPPERSONEL",
+                newName: "GOREVKODU");
+
+            migrationBuilder.RenameColumn(
+                name: "CILTNO",
+                table: "MPPERSONEL",
+                newName: "GOREVI");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NUFUSCUZDANVERILISTARIH",
+                table: "MPPERSONEL",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CİLTNO",
+                table: "MPPERSONEL",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
