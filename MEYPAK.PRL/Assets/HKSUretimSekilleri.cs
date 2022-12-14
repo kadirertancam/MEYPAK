3170385b9e960dcc46c5867dc7b5259349574d5e
kadir ertan çam <kadirertan1789@gmail.com>
2022-12-14 17:04:10 +0300

    Birleştirme

---
+++ MEYPAK.PRL/Assets/HKSUretimSekilleri.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEYPAK.PRL.Assets
+{
+    public class HKSUretimSekilleri
+    {
+
+        // NOT: Oluşturulan kod en az .NET Framework 4.5 veya.NET Core/Standard 2.0 gerektirebilir.
+        /// <remarks/>
+        [System.SerializableAttribute()]
+        [System.ComponentModel.DesignerCategoryAttribute("code")]
+        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+        [System.Xml.Serialization.XmlRootAttribute(Namespace = "http://schemas.xmlsoap.org/soap/envelope/", IsNullable = false)]
+        public partial class Envelope
+        {
+
+            private EnvelopeBody bodyField;
+
+            /// <remarks/>
+            public EnvelopeBody Body
+            {
+                get
+                {
+                    return this.bodyField;
+                }
+                set
+                {
+                    this.bodyField = value;
+                }
+            }
+        }
+
+        /// <remarks/>
+        [System.SerializableAttribute()]
+        [System.ComponentModel.DesignerCategoryAttribute("code")]
+        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+        public partial class EnvelopeBody
+        {
+
+            private BaseResponseMessageOf_UretimSekilleriCevap baseResponseMessageOf_UretimSekilleriCevapField;
+
+            /// <remarks/>
+            [System.Xml.Serialization.XmlElementAttribute(Namespace = "http://www.gtb.gov.tr//WebServices")]
+            public BaseResponseMessageOf_UretimSekilleriCevap BaseResponseMessageOf_UretimSekilleriCevap
+            {
+                get
+                {
+                    return this.baseResponseMessageOf_UretimSekilleriCevapField;
+                }
+                set
+                {
+                    this.baseResponseMessageOf_UretimSekilleriCevapField = value;
+                }
+            }
+        }
+
+        /// <remarks/>
+        [System.SerializableAttribute()]
+        [System.ComponentModel.DesignerCategoryAttribute("code")]
+        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://www.gtb.gov.tr//WebServices")]
+        [System.Xml.Serialization.XmlRootAttribute(Namespace = "http://www.gtb.gov.tr//WebServices", IsNullable = false)]
+        public partial class BaseResponseMessageOf_UretimSekilleriCevap
+        {
+
+            private object hataKodlariField;
+
+            private string islemKoduField;
+
+            private BaseResponseMessageOf_UretimSekilleriCevapSonuc sonucField;
+
+            /// <remarks/>
+            [System.Xml.Serialization.XmlElementAttribute(IsNullable = true)]
+            public object HataKodlari
+            {
+                get
+                {
+                    return this.hataKodlariField;
+                }
+                set
+                {
+                    this.hataKodlariField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string IslemKodu
+            {
+                get
+                {
+                    return this.islemKoduField;
+                }
+                set
+                {
+                    this.islemKoduField = value;
+                }
+            }
+
+            /// <remarks/>
+            public BaseResponseMessageOf_UretimSekilleriCevapSonuc Sonuc
+            {
+                get
+                {
+                    return this.sonucField;
+                }
+                set
+                {
+                    this.sonucField = value;
+                }
+            }
+        }
+
+        /// <remarks/>
+        [System.SerializableAttribute()]
+        [System.ComponentModel.DesignerCategoryAttribute("code")]
+        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://www.gtb.gov.tr//WebServices")]
+        public partial class BaseResponseMessageOf_UretimSekilleriCevapSonuc
+        {
+
+            private byte hataKoduField;
+
+            private object mesajField;
+
+            private UretimSekliDTO[] uretimSekilleriField;
+
+            /// <remarks/>
+            [System.Xml.Serialization.XmlElementAttribute(Namespace = "http://schemas.datacontract.org/2004/07/GTB.HKS.Urun.ServiceContract")]
+            public byte HataKodu
+            {
+                get
+                {
+                    return this.hataKoduField;
+                }
+                set
+                {
+                    this.hataKoduField = value;
+                }
+            }
+
+            /// <remarks/>
+            [System.Xml.Serialization.XmlElementAttribute(Namespace = "http://schemas.datacontract.org/2004/07/GTB.HKS.Urun.ServiceContract", IsNullable = true)]
+            public object Mesaj
+            {
+                get
+                {
+                    return this.mesajField;
+                }
+                set
+                {
+                    this.mesajField = value;
+                }
+            }
+
+            /// <remarks/>
+            [System.Xml.Serialization.XmlArrayAttribute(Namespace = "http://schemas.datacontract.org/2004/07/GTB.HKS.Urun.ServiceContract")]
+            [System.Xml.Serialization.XmlArrayItemAttribute("UretimSekliDTO", Namespace = "http://schemas.datacontract.org/2004/07/GTB.HKS.Urun.Model", IsNullable = false)]
+            public UretimSekliDTO[] UretimSekilleri
+            {
+                get
+                {
+                    return this.uretimSekilleriField;
+                }
+                set
+                {
+                    this.uretimSekilleriField = value;
+                }
+            }
+        }
+
+        /// <remarks/>
+        [System.SerializableAttribute()]
+        [System.ComponentModel.DesignerCategoryAttribute("code")]
+        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://schemas.datacontract.org/2004/07/GTB.HKS.Urun.Model")]
+        [System.Xml.Serialization.XmlRootAttribute(Namespace = "http://schemas.datacontract.org/2004/07/GTB.HKS.Urun.Model", IsNullable = false)]
+        public partial class UretimSekliDTO
+        {
+
+            private byte idField;
+
+            private string uretimSekliAdiField;
+
+            /// <remarks/>
+            public byte Id
+            {
+                get
+                {
+                    return this.idField;
+                }
+                set
+                {
+                    this.idField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string UretimSekliAdi
+            {
+                get
+                {
+                    return this.uretimSekliAdiField;
+                }
+                set
+                {
+                    this.uretimSekliAdiField = value;
+                }
+            }
+        }
+
+        /// <remarks/>
+        [System.SerializableAttribute()]
+        [System.ComponentModel.DesignerCategoryAttribute("code")]
+        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://schemas.datacontract.org/2004/07/GTB.HKS.Urun.ServiceContract")]
+        [System.Xml.Serialization.XmlRootAttribute(Namespace = "http://schemas.datacontract.org/2004/07/GTB.HKS.Urun.ServiceContract", IsNullable = false)]
+        public partial class UretimSekilleri
+        {
+
+            private UretimSekliDTO[] uretimSekliDTOField;
+
+            /// <remarks/>
+            [System.Xml.Serialization.XmlElementAttribute("UretimSekliDTO", Namespace = "http://schemas.datacontract.org/2004/07/GTB.HKS.Urun.Model")]
+            public UretimSekliDTO[] UretimSekliDTO
+            {
+                get
+                {
+                    return this.uretimSekliDTOField;
+                }
+                set
+                {
+                    this.uretimSekliDTOField = value;
+                }
+            }
+        }
+
+
+    }
+}
