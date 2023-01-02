cb3b013528d249cd22f722187353d91c9f649548
seydayildiz <seydayildiz1206@gmail.com>
2023-01-02 13:40:36 +0300

    MEYPAKWeb tema giydirme işlemi yapıldı. Login kısmı düzenlendi.

---
+++ MEYPAK.WEB/Controllers/DefaultController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MEYPAK.WEB.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
