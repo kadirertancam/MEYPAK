c494429523cd350f96f9aa4848fdd90951741384
efebaslilar <efebaslilar@gmail.com>
2023-04-06 10:42:02 +0300

    06.04.2023 // Versiyon Kontrol API

---
+++ MEYPAK.API/Controllers/PARAMETREControllers/VersiyonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MEYPAK.API.Controllers.PARAMETREControllers
+{
+    public class VersiyonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok("1.2.0.1");
+        }
+    }
+}
